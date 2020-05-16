f2828533ed0d3982d1818e7944ac63255d47a9fa
qwaszx000 <kalita1303@gmail.com>
2020-05-16 13:08:32 +0300

    models update

--- api/admin.py
+++ api/admin.py
@@ -4,11 +4,22 @@ from delivery_django.api.models import *
 # Register your models here.
  
 admin.site.register(Dish)
-admin.site.register(Restaurant)
 admin.site.register(Topping)
 admin.site.register(Accompaniments)
+admin.site.register(Modification)
+admin.site.register(Taxe)
+admin.site.register(Package)
+admin.site.register(CuisineType)
+admin.site.register(DishTag)
+
+admin.site.register(Restaurant)
 admin.site.register(Order)
 admin.site.register(TimePeriod)
+admin.site.register(OrderPosition)
+
 admin.site.register(Client)
 admin.site.register(DeliveryAddress)
 admin.site.register(CreditCard)
+
+admin.site.register(Manager)
+admin.site.register(Courier)
--- api/models.py
+++ api/models.py
@@ -9,25 +9,54 @@ class Dish(models.Model):
 	Restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
 	DeliveryPrice = models.FloatField()
 
+	Tags = models.ManyToManyField(DishTag)
+	
+	#cost
+	PurchasePrice = models.FloatField()
+	SalePrice = models.FloatField()
+	Taxe = models.ForeignKey(Taxe, on_delete=models.CASCADE)
+
+	#modificators
+	Accompaniments = models.ManyToManyField(Accompaniment)
+	Toppings = models.ManyToManyField(Topping)
+	Modifications = models.ManyToManyField(Modification)
+	Packages = models.ManyToManyField(Package)
+
+	#limits
+	LimitPerOrder = models.SmallIntegerField()
+	TimePeriod = models.OneToOneField(TimePeriod, on_delete=models.CASCADE)
+
+	DarkKitchen = models.BooleanField()
+
+	#img
+	ImgPopup = models.ImageField(upload_to="uploads/img/popup/")
+	ImgMenuPage = models.ImageField(upload_to="uploads/img/menu_page/")
+
 class Restaurant(models.Model):
 	EnglishName = models.CharField(max_length=128)
 	SecondName = models.CharField(max_length=128)
+	#geo
 	Address = models.CharField(max_length=512)
 	CoordinatesLongtitude = models.FloatField()
 	CoordinatesLatitude = models.FloatField()
+
 	Phone = models.CharField(max_length=16)
 	DiscountGiven = models.BooleanField()
 	StandartCookingTime = models.SmallIntegerField()
+	#more info
 	MoreInfoEnglish = models.TextField()
 	MoreInfoSecond = models.TextField()
+	#seo
 	SeoSectionEnglish = models.TextField()
 	SeoSectionSecond = models.TextField()
+	#types
 	Type = models.CharField(max_length=12)#simple/dark-kitchen/both
-	#TypeOfCusine = 
-	SliderImgBigEng = models.ImageField(upload_to="uploads/eng/big/")
-	SliderImgSmallEng = models.ImageField(upload_to="uploads/eng/small/")
-	SliderImgBigSecond = models.ImageField(upload_to="uploads/second/big/")
-	SliderImgSmallSecond = models.ImageField(upload_to="uploads/second/small")
+	TypesOfCusine = models.ManyToManyField(CuisineType)
+	#imgs
+	SliderImgBigEng = models.ImageField(upload_to="uploads/img/eng/big/")
+	SliderImgSmallEng = models.ImageField(upload_to="uploads/img/eng/small/")
+	SliderImgBigSecond = models.ImageField(upload_to="uploads/img/second/big/")
+	SliderImgSmallSecond = models.ImageField(upload_to="uploads/img/second/small")
 
 #additional ingredient
 class Topping(models.Model):
@@ -37,39 +66,17 @@ class Topping(models.Model):
 	PriceSell = models.FloatField()
 
 #snack
-class Accompaniments(models.Model)
+class Accompaniment(models.Model)
 	EnglishName = models.CharField(max_length=128)
 	SecondName = models.CharField(max_length=128)
 	PricePurchase = models.FloatField()
 	PriceSell = models.FloatField()
 
-#contains OrderPosition
-class Order(models.Model):
-	#if client removed - remove Order releated with him
-	#many Order to one Client
-	Client = models.ForeignKey(Client, on_delete=models.CASCADE)
-
-	#if paytype is creditcard - payinfo will get card id
-	PayType = models.CharField(max_length=128)
-	PayInfo = models.CharField(max_length=128)
-	#many orders to one delivery address
-	DeliveryAddress = models.ForeignKey(DeliveryAddress, on_delete=models.SET_NULL)
-	#чаевые
-	TipsCount = models.FloatField()
-	TotalCost = models.FloatField()
-	DeliveryCost = models.FloatField()
-	AddingDate = models.DateTimeField(auto_now_add=True)
-	#на когда заказали
-	OnWhenDate = models.DateTimeField()
-	DeliveredDate = models.DateTimeField()
-	Status = models.CharField(max_length=128)
-	TimePeriod = models.OneToOneField(TimePeriod, on_delete=models.CASCADE)
-	#CourierTakedMoney = 
-
-#for 1 dish
-class OrderPosition(models.Model):
-	#many OrderPosition to one order
-	Order = models.ForeignKey(Order, on_delete=models.CASCADE)
+#example - roasting level for meat
+class Modification(models.Model):
+	EnglishName = models.CharField(max_length=128)#roasting level for meat
+	SecondName = models.CharField(max_length=128)  
+	Value = models.CharField(max_length=128) #medium
 
 class TimePeriod(models.Model):
 	# use json.
@@ -87,6 +94,58 @@ class TimePeriod(models.Model):
 	StartTime = models.CharField(max_length=128)
 	StopTime = models.CharField(max_length=128)
 
+#contains OrderPosition
+class Order(models.Model):
+	#if client removed - remove Order releated with him
+	#many Order to one Client
+	Client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+	#if paytype is creditcard - payinfo will get card id
+	PayType = models.CharField(max_length=128)
+	PayInfo = models.CharField(max_length=128)
+	#many orders to one delivery address
+	DeliveryAddress = models.ForeignKey(DeliveryAddress, on_delete=models.SET_NULL)
+	#чаевые
+	TipsCount = models.FloatField()
+	TotalCost = models.FloatField()
+	DeliveryCost = models.FloatField()
+
+	AddingDate = models.DateTimeField(auto_now_add=True)
+	#на когда заказали
+	OnWhenDate = models.DateTimeField()
+	DeliveredDate = models.DateTimeField()
+
+	TimePeriod = models.OneToOneField(TimePeriod, on_delete=models.CASCADE)
+	CourierTakedMoney = models.OneToOneField(Courier, on_delete=models.SET_NULL)
+
+	Status = models.CharField(max_length=128)
+	#Status can be: new, sent_to_kitchen, in_cook_process, in_delivery_process, delivered, rejected, delayed
+
+#for 1 dish
+class OrderPosition(models.Model):
+	Dish = models.OneToOneField(Dish, on_delete=models.CASCADE)
+	#many OrderPosition to one order and restourants
+	Order = models.ForeignKey(Order, on_delete=models.CASCADE)
+	Restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+	#price
+	BuyPrice = models.FloatField()
+	SellPrice = models.FloatField()
+	Discount = models.FloatField()
+	Taxe = models.ForeignKey(Taxe, on_delete=models.CASCADE)
+
+	Courier = models.ForeignKey(Courier, on_delete=models.SET_NULL)
+
+	#datetimes
+	AddingDate = models.DateTimeField(auto_now_add=True)
+	SendToRestourantDate = models.DateTimeField(auto_now=True)
+	RestourantConfirmDate = models.DateTimeField(auto_now=True)
+	RestourantDoneDate = models.DateTimeField(auto_now=True)
+	CourierTakedDate = models.DateTimeField(auto_now=True)
+	CourierDeliveredDate = models.DateTimeField(auto_now=True)
+
+	Status = models.CharField(max_length=128)
+	#Status can be: new, sent_to_kitchen, in_cook_process, in_delivery_process, delivered, rejected, delayed
+
 # https://habr.com/ru/post/313764/
 # https://docs.djangoproject.com/en/3.0/topics/auth/customizing/
 #using one to one connection
@@ -100,6 +159,12 @@ class Client(models.Model):
 	#Automatically set the field to now when the object is first created.
 	RegisterData = models.DateTimeField(auto_now_add=True)
 	LastLoginDate = models.DateTimeField(auto_now_add=True)
+
+class Manager(models.Model):
+	User = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Courier(models.Model):
+	User = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class DeliveryAddress(models.Model):
 	#if client removed - remove address releated with him
@@ -131,3 +196,13 @@ class Package(models.Model):
 	Name = models.CharField(max_length=128)
 	Price = models.FloatField()
 	SalePrice = models.FloatField()
+
+
+
+class CuisineType(models.Model):
+	EnglishName = models.CharField(max_length=128)
+	SecondName = models.CharField(max_length=128)
+
+class DishTag(models.Model):
+	EnglishName = models.CharField(max_length=128)
+	SecondName = models.CharField(max_length=128)
